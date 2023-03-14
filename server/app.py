4276138df518635135c6bfb4a10834bcb037afa8
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-03-14 16:00:51 -0400

    passed tests

--- server/app.py
+++ server/app.py
@@ -17,11 +17,47 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_list = [p.to_dict() for p in Plant.query.all()]
+
+        response = make_response(
+            plants_list,
+            200
+        )
+
+        return response
+    
+    def post(self):
+        new_plant = Plant(
+            name=request.json['name'],
+            image=request.json['image'],
+            price=request.json['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response = make_response(
+            new_plant.to_dict(),
+            201
+        )
+
+        return response
+
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter(Plant.id == id).first().to_dict()
+
+        response = make_response(
+            plant,
+            200
+        )
+
+        return response
         
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
